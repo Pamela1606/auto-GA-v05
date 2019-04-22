d43e49c48310251b1f1ef1e9a24688833b717286
PamelaCardozo <pam16card@gmail.com>
2019-04-22 08:18:17 -0700

    second commit

--- src/main/java/org/umssdiplo/automationv01/core/utils/adidas/Adidas.java
+++ src/main/java/org/umssdiplo/automationv01/core/utils/adidas/Adidas.java
@@ -1,13 +1,9 @@
 package org.umssdiplo.automationv01.core.utils.adidas;
 
 import org.umssdiplo.automationv01.core.customwebdriver.ManageDriver;
-import org.umssdiplo.automationv01.core.managepage.care4you.web.Incident.IncidentCreate;
-import org.umssdiplo.automationv01.core.managepage.care4you.web.Incident.IncidentDelete;
-import org.umssdiplo.automationv01.core.managepage.care4you.web.Incident.IncidentEdit;
-import org.umssdiplo.automationv01.core.managepage.care4you.web.Incident.IncidentHome;
-import org.umssdiplo.automationv01.core.managepage.care4you.web.home.Home;
+import org.umssdiplo.automationv01.core.managepage.webadidas.Home;
 import org.umssdiplo.automationv01.core.utils.PropertyAccessor;
-import org.umssdiplo.automationv01.core.utils.WebUrls;
+
 
 public final class Adidas {
     private Home home;
@@ -15,35 +11,18 @@ public final class Adidas {
 
     private Adidas() {
         home = new Home();
-        incidentHome = new IncidentHome();
-        incidentCreate = new IncidentCreate();
-        incidentEdit = new IncidentEdit();
-        incidentDelete = new IncidentDelete();
+
     }
 
-    public static Care4You init() {
+    public static Adidas init() {
         ManageDriver.getInstance().getWebDriver()
-                .navigate().to(PropertyAccessor.getInstance().getBaseUrl(WebUrls.CARE4YOU));
-        return new Care4You();
+                .navigate().to(PropertyAccessor.getInstance().getBaseUrl());
+        return new Adidas();
     }
 
     public Home getHome() {
         return home;
     }
 
-    public IncidentHome getIncidentHome() {
-        return incidentHome;
-    }
 
-    public IncidentCreate getIncidentCreate() {
-        return incidentCreate;
-    }
-
-    public IncidentEdit getIncidentEdit() {
-        return incidentEdit;
-    }
-
-    public IncidentDelete getIncidentDelete() {
-        return incidentDelete;
-    }
 }
